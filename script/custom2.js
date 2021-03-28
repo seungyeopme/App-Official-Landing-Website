ca1dc4fd3c129f5ca554b969600474d372297ed2
seungyeopme <seungyeop.me.dev@gmail.com>
2021-03-28 21:38:45 +0900

    함수

---
+++ script/custom2.js
@@ -0,0 +1,12 @@
+$('.show-btn').click(function(){
+    $('div').show()
+})
+$('.hide-btn').click(function() {
+    $('div').hide()
+})
+$('.btn').mouseenter(function() {
+    $('div').show()
+})
+$('.btn').mouseleave(function() {
+    $('div').hide()
+})
