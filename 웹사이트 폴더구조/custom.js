8be9239d9d3a84a1356592f572a670ad1f1b28e1
seungyeopme <seungyeop.me.dev@gmail.com>
2021-04-21 00:57:13 +0900

    welcome 섹션 제작(슬라이더)

---
+++ 웹사이트 폴더구조/custom.js
@@ -0,0 +1,12 @@
+$(function(){
+    /* Slick Slider - Welcome */
+    $('.slideshow').slick({
+        infinite: true,
+        dots: false,
+        autoplay: true,
+        autoplayuSpeed: 2000,
+        fade: true,
+        speed: 1000,
+        pauseOnHover: false
+    });
+})
